3d81946cc7e5589523fe5f08b494a0f6f492d423
serg <sergkop@gmail.com>
2012-01-27 14:50:51 +0000

    form fix

--- observers/forms.py
+++ observers/forms.py
@@ -1,10 +1,13 @@
-from django.forms import ModelForm
+from django import forms
 
 from captcha.fields import CaptchaField
 
 from observers.models import Observer
 
-class ObserverSignupForm(ModelForm):
+class ObserverSignupForm(forms.ModelForm):
+    email = forms.CharField(max_length=50, required=False)
+    info = forms.CharField(required=False, widget=forms.Textarea())
+    in_moscow = forms.BooleanField(required=False)
     #captcha = CaptchaField()
 
     class Meta:
