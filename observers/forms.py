9f70430bf2f314e01aa2f2f8174d657dbe4b146d
serg <sergkop@gmail.com>
2012-01-28 13:52:17 +0000

    sending emails

--- observers/forms.py
+++ observers/forms.py
@@ -7,8 +7,9 @@ from observers.models import Observer
 class ObserverSignupForm(forms.ModelForm):
     email = forms.CharField(max_length=50, required=False)
     info = forms.CharField(required=False, widget=forms.Textarea())
-    in_moscow = forms.BooleanField(required=False)
+    #in_moscow = forms.BooleanField(required=False)
     #captcha = CaptchaField()
 
     class Meta:
         model = Observer
+        exclude = ('in_moscow',)
--- observers/views.py
+++ observers/views.py
@@ -1,3 +1,8 @@
+# coding=utf8
+from smtplib import SMTPException
+
+from django.conf import settings
+from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
@@ -9,7 +14,26 @@ def signup(request):
         form = ObserverSignupForm(request.POST)
 
         if form.is_valid():
-            form.save()
+            observer = form.save(commit=False)
+            observer.in_moscow = request.POST.get('in_moscow', 'yes')=='yes'
+            observer.save()
+
+            if observer.in_moscow:
+                email = settings.MOSCOW_OBSERVERS_EMAIL
+            else:
+                email = settings.REGION_OBSERVERS_EMAIL
+
+            message = u'ФИО: ' + observer.full_name + '\n'
+            message += u'Телефон: ' + observer.telephone + '\n'
+            message += u'Адрес: ' + observer.location + '\n'
+            message += u'Email: ' + observer.email + '\n'
+            message += u'Дополнительно: ' + observer.info + '\n'
+
+            try:
+                send_mail(u'Новый наблюдатель', message, 'gn@nabludatel.org', [email], fail_silently=False)
+            except SMTPException:
+                pass # TODO: write observer id to a special file
+
             return HttpResponseRedirect('/spasibo/')
     else:
         form = ObserverSignupForm()
